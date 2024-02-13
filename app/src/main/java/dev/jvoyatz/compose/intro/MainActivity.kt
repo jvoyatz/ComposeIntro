942a7a559376e2042e09ccc1b1dcdfebc3666218
jvoyatz <jvoyatz@gmail.com>
2024-02-13 07:48:35 +0200

    custom button -- google example

--- app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
+++ app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
@@ -1,183 +1,50 @@
 package dev.jvoyatz.compose.intro
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import dev.jvoyatz.compose.intro.ui.theme.ComposeIntroTheme
-import dev.jvoyatz.compose.intro.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeIntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Greeting()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    GoogleButton(
+                        text = "Sign in with Google",
+                        loadingText = "Signing in",
+                        onClicked = {
+                            Log.d("main activitiy", "onClicked: clicked")
+                        }
+                    )
                 }
             }
         }
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Greeting() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var text by remember {
-            mutableStateOf("Type here...")
-        }
-        BasicTextField(
-            modifier = Modifier.background(Color.LightGray).padding(16.dp),
-            value = text,
-            onValueChange = {
-                text = it
-            },
-            enabled = true,
-            readOnly = false,
-//            label = {
-//                Text(text = "Title")
-//            },
-            singleLine = true,
-//            leadingIcon = {
-//                IconButton(onClick = { /*TODO*/ }) {
-//                    Icon(
-//                        imageVector = Icons.Filled.Email,
-//                        contentDescription = "email"
-//                    )
-//                }
-//            },
-//            trailingIcon = {
-//                IconButton(onClick = { Log.d("compose", "Greeting: trailing ") }) {
-//                    Icon(
-//                        imageVector = Icons.Filled.Check,
-//                        contentDescription = "check"
-//                    )
-//                }
-//            },
-
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = { Log.d("compose", "Greeting: ime action search")}
-            )
-        )
-    }
-
-}
-
-//   OutlinedTextField(
-//            value = text,
-//            onValueChange = {
-//                text = it
-//            },
-//            enabled = true,
-//            readOnly = false,
-//            label = {
-//                Text(text = "Title")
-//            },
-//            singleLine = true,
-//            leadingIcon = {
-//                IconButton(onClick = { /*TODO*/ }) {
-//                    Icon(
-//                        imageVector = Icons.Filled.Email,
-//                        contentDescription = "email"
-//                    )
-//                }
-//            },
-//            trailingIcon = {
-//                IconButton(onClick = { Log.d("compose", "Greeting: trailing ") }) {
-//                    Icon(
-//                        imageVector = Icons.Filled.Check,
-//                        contentDescription = "check"
-//                    )
-//                }
-//            },
-//
-//            keyboardOptions = KeyboardOptions(
-//                keyboardType = KeyboardType.Email,
-//                imeAction = ImeAction.Search
-//            ),
-//            keyboardActions = KeyboardActions(
-//                onSearch = { Log.d("compose", "Greeting: ime action search")}
-//            )
-//        )
-//    }
-
-
 @Preview (showBackground = true)
 @Composable
 fun DefaultPreview(){
     ComposeIntroTheme {
         Column(modifier = Modifier.fillMaxSize()) {
-            Greeting()
+
         }
     }
 }

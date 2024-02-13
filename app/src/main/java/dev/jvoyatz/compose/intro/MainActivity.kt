07f4ea5b53b2fb6a4815c816ada5a30312695d27
jvoyatz <jvoyatz@gmail.com>
2024-02-13 08:12:34 +0200

    coil-1.4.0 - issues

--- app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
+++ app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
@@ -5,15 +5,29 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import dev.jvoyatz.compose.intro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,15 +40,43 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    GoogleButton(
-                        text = "Sign in with Google",
-                        loadingText = "Signing in",
-                        onClicked = {
-                            Log.d("main activitiy", "onClicked: clicked")
-                        }
-                    )
+                    CoilImage()
                 }
             }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun CoilImage() {
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp), contentAlignment = Alignment.Center
+    ) {
+
+
+        val painter = rememberImagePainter(
+            data = "https://avatars.githubusercontent.com/u/14994036v=4",
+            builder = {
+                allowHardware(false)
+                placeholder(R.drawable.ic_launcher_background)
+                error(R.drawable.ic_google_logo)
+                crossfade(1000)
+                transformations(
+                    GrayscaleTransformation(),
+                    //CircleCropTransformation(),
+                    BlurTransformation(LocalContext.current),
+                    RoundedCornersTransformation(50f)
+                )
+            })
+
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "image")
+        if(painterState is ImagePainter.State.Loading) {
+            CircularProgressIndicator()
         }
     }
 }

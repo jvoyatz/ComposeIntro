62e6b2a0b6fc60870b5f77241f5653b82a1733cc
jvoyatz <jvoyatz@gmail.com>
2024-02-18 16:17:56 +0200

    password text field

--- app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
+++ app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
@@ -12,13 +12,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
@@ -31,6 +45,7 @@ import coil.transform.RoundedCornersTransformation
 import dev.jvoyatz.compose.intro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,53 +55,57 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    CoilImage()
+                    Greeting()
                 }
             }
         }
     }
 }
 
-
-@OptIn(ExperimentalCoilApi::class)
+@ExperimentalMaterial3Api
 @Composable
-fun CoilImage() {
-    Box(
-        modifier = Modifier
-            .height(150.dp)
-            .width(150.dp), contentAlignment = Alignment.Center
-    ) {
-
-
-        val painter = rememberImagePainter(
-            data = "https://avatars.githubusercontent.com/u/14994036v=4",
-            builder = {
-                allowHardware(false)
-                placeholder(R.drawable.ic_launcher_background)
-                error(R.drawable.ic_google_logo)
-                crossfade(1000)
-                transformations(
-                    GrayscaleTransformation(),
-                    //CircleCropTransformation(),
-                    BlurTransformation(LocalContext.current),
-                    RoundedCornersTransformation(50f)
-                )
-            })
-
-        val painterState = painter.state
-        Image(painter = painter, contentDescription = "image")
-        if(painterState is ImagePainter.State.Loading) {
-            CircularProgressIndicator()
+fun Greeting() {
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        var password by rememberSaveable {
+            mutableStateOf("")
         }
+
+        var passwordvis by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        val icon = if(passwordvis) {
+            painterResource(id = R.drawable.baseline_visibility_24)
+        } else {
+            painterResource(id = R.drawable.baseline_visibility_off_24)
+        }
+
+        OutlinedTextField(value = password, onValueChange = { password = it },
+            placeholder = { Text(text = "password") },
+            label = { Text(text = "password") },
+            trailingIcon = {
+                IconButton(onClick = { passwordvis = !passwordvis }) {
+                    Icon(icon, contentDescription = null)
+                }
+            },
+            visualTransformation = if(passwordvis) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            )
+        )
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview (showBackground = true)
 @Composable
 fun DefaultPreview(){
     ComposeIntroTheme {
         Column(modifier = Modifier.fillMaxSize()) {
-
+            Greeting()
         }
     }
 }

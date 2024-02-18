622db3fca3234fc12583a5eb8208d25763491452
jvoyatz <jvoyatz@gmail.com>
2024-02-18 17:36:10 +0200

    lazy column

--- app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
+++ app/src/main/java/dev/jvoyatz/compose/intro/MainActivity.kt
@@ -5,14 +5,21 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -45,33 +52,51 @@ import coil.transform.BlurTransformation
 import coil.transform.CircleCropTransformation
 import coil.transform.GrayscaleTransformation
 import coil.transform.RoundedCornersTransformation
+import dev.jvoyatz.compose.intro.repositories.PersonRepository
 import dev.jvoyatz.compose.intro.ui.theme.ComposeIntroTheme
+import kotlin.text.Typography.section
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeIntroTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Row(
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        GradientButton(text = "button", textColor =
-                            Color.White, gradient = Brush.horizontalGradient(
-                                colors = listOf(
-                                    Color.Green,
-                                    Color.Blue
-                                )
-                            )) {
-                            
+
+                val sections = listOf<String>("a", "b", "c", "d", "e", "f", "g")
+
+                val personRepository = PersonRepository()
+                val getAllData = personRepository.getAllData()
+
+                LazyColumn(contentPadding = PaddingValues(
+                    all = 12.dp
+                ),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)) {
+
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(text = "section -> $section",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Color.LightGray)
+                                    .padding(12.dp))
+                        }
+                        items(10) {
+
+                            Text(text = "item $it fromm the section $section",
+                                modifier = Modifier.padding(12.dp)
+                            )
                         }
                     }
+
+
+//                    itemsIndexed(items = getAllData,
+//                        key = { index, person ->
+//                            person.id
+//                        }) {i, p ->
+//                        Log.d("tag index", "onCreate: $i")
+//                        CustomItem(person = p)
+//                    }
                 }
             }
         }
---
+++ app/src/main/java/dev/jvoyatz/compose/intro/models/Person.kt
@@ -0,0 +1,8 @@
+package dev.jvoyatz.compose.intro.models
+
+data class Person(
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
